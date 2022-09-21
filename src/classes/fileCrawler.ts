d027be43032669c0a0086eff56bda924a0340cd0
simekh <dev@simonekholm.se>
2022-09-21 22:39:31 +0200

    Remove "Absolute" from variables and functions

--- src/classes/fileCrawler.ts
+++ src/classes/fileCrawler.ts
@@ -31,14 +31,14 @@ export class fileCrawler {
   }
 
   private moveFileToNewDestination(identifierPath: string): void {
-    const destinationAbsoluteDir = this.getFileAbsoluteDestination(identifierPath);
-    const newFileAbsolutePath = this.getNewFileAbsolutePath(destinationAbsoluteDir);
-    if (this.fileHasMoved(this.filePath, newFileAbsolutePath)) {
-      this.moveFile(newFileAbsolutePath, true);
+    const destinationDir = this.getFileDestination(identifierPath);
+    const newFilePath = this.getNewFilePath(destinationDir);
+    if (this.fileShouldMove(newFilePath)) {
+      this.moveFile(newFilePath, true);
     }
   }
 
-  private getFileAbsoluteDestination(line: string): string {
+  private getFileDestination(line: string): string {
     const destinationPath = this.getDestinationFromIdentifier(line);
     return this.workingDirPath + destinationPath;
   }
@@ -47,19 +47,20 @@ export class fileCrawler {
     return line.split(this.DESTINATIONIDENTIFIER)[1];
   }
 
-  private fileHasMoved(oldPath: string, newPath: string): boolean {
+  private fileShouldMove(newPath: string): boolean {
+    var oldPath = this.filePath;
     return oldPath !== newPath;
   }
 
-  private getNewFileAbsolutePath(destinationAbsoluteDir: string): string {
+  private getNewFilePath(destinationDir: string): string {
     const filename = basename(this.filePath);
-    return destinationAbsoluteDir + filename;
+    return destinationDir + filename;
   }
 
   private moveFile(newFilePath: string, isSalesforceClsFile: boolean): void {
-    const newFileAbsoluteDir = dirname(newFilePath);
-    if (!existsSync(newFileAbsoluteDir)) {
-      mkdirSync(newFileAbsoluteDir, { recursive: true });
+    const newFileDir = dirname(newFilePath);
+    if (!existsSync(newFileDir)) {
+      mkdirSync(newFileDir, { recursive: true });
     }
 
     rename(this.filePath, newFilePath, (err) => {
@@ -72,14 +73,14 @@ export class fileCrawler {
     }
   }
   private moveMetaXmlFile(newFilePath: string): void {
-    const oldMetaFileAbsolutePath = `${this.filePath}-meta.xml`;
-    const newMetaFileAbsolutePath = `${newFilePath}-meta.xml`;
+    const oldMetaFilePath = `${this.filePath}-meta.xml`;
+    const newMetaFilePath = `${newFilePath}-meta.xml`;
 
-    rename(oldMetaFileAbsolutePath, newMetaFileAbsolutePath, (err) => {
+    rename(oldMetaFilePath, newMetaFilePath, (err) => {
       if (err) {
         throw err;
       }
     });
-    console.log(`Moved "${oldMetaFileAbsolutePath}" to "${newMetaFileAbsolutePath}" `);
+    console.log(`Moved "${oldMetaFilePath}" to "${newMetaFilePath}" `);
   }
 }
